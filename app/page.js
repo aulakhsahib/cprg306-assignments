838cd4e51142749d5e457cd2e68a8c5647726918
aulakhsahib <sahibaulakh2003@gmail.com>
2024-01-30 10:55:39 -0700

    added the week-3 page

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="./week-2" className="underline">Week 2</Link>
+      <Link href="./week-3" className="underline">Week 3</Link>
     </div>
   );
 }

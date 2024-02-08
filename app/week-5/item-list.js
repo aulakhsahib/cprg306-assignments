1d074ddb4fd4f0ddd9c5859338156415f8c9b663
aulakhsahib <sahibaulakh2003@gmail.com>
2024-02-07 19:53:13 -0700

    added groupBy

---
+++ app/week-5/item-list.js
@@ -0,0 +1,19 @@
+import Item from "./item";
+
+export default function ItemList() {
+
+    
+    
+    return (
+        // <ul className="grid gap-y-4 w-11/12 my-6 mx-auto">
+        //     {dataList.map((data, index) => {
+        //         return (
+        //             <li className="bg-blue-900 rounded-lg p-4 w-6/12 max-w-xs" key={index}>
+        //                 <Item {...data} />
+        //             </li>
+        //         );
+        //     })}
+        // </ul>
+        <p>Hello</p>
+    );
+};
